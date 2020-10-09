01bf5b3cd24a3db1400e64b31bd25a2de24f6585
Eric Li <eric_li@moma.org>
2020-10-09 14:05:58 -0400

    release script

--- bin/release.sh
+++ bin/release.sh
@@ -5,10 +5,10 @@ if [ ! -f "./node_modules/.bin/webpack" ]; then
   yarn install
 fi
 
+git checkout master
 git checkout --detach
 yarn build
 git commit -m $1
 git tag v$1
 git push --tags
 git checkout master
-# continue development
