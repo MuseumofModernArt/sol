74db51f3c639cc87d910f46cce71577895ebc4a3
Eric Li <eric_li@moma.org>
2020-10-09 14:25:49 -0400

    updates to release

--- bin/release.sh
+++ bin/release.sh
@@ -5,6 +5,9 @@ if [ ! -f "./node_modules/.bin/webpack" ]; then
   yarn install
 fi
 
+# this follows the jQuery release technique. derived from: https://stackoverflow.com/a/50974956
+# this is temporary until we migrate to the MuseumofModernArt GH org, and can make use of npm registries
+
 git checkout master
 git checkout --detach
 yarn build
