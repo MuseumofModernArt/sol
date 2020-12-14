1385c086fce72d9f3c8868624e12a9cc43818687
Eric Li <eric_li@moma.org>
2020-12-14 14:35:30 -0500

    get the base domain for setting cookies for .moma.org instead of www.moma.org or staging.moma.org

--- src/js/util/util.js
+++ src/js/util/util.js
@@ -33,9 +33,26 @@ const GetElementsList = (elements) => {
   return elsArray;
 }
 
+// Loops through the parts of a hostname and tries to set a cookie on that domain, returning the highest level possible.
+// Derived from http://rossscrivener.co.uk/blog/javascript-get-domain-exclude-subdomain
+const BaseDomain = () => {
+  if (!document.domain) {
+    return '';
+  }
+
+  let i=0,domain=document.domain,p=domain.split('.'),s='_gd'+(new Date()).getTime();
+  while(i<(p.length-1) && document.cookie.indexOf(s+'='+s)==-1){
+    domain = p.slice(-1-(++i)).join('.');
+    document.cookie = s+"="+s+";domain="+domain+";";
+  }
+  document.cookie = s+"=;expires=Thu, 01 Jan 1970 00:00:01 GMT;domain="+domain+";";
+  return domain;
+}
+
 export {
   IsMobileOrTablet,
   DocumentReady,
   WindowHasSize,
   GetElementsList,
+  BaseDomain,
 }
