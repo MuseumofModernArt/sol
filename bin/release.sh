b1bda506644a53f9942af0953cd47e6f56488fcd
Eric Li <eric_li@moma.org>
2021-04-12 16:20:07 -0400

    update release script

--- bin/release.sh
+++ bin/release.sh
@@ -8,6 +8,7 @@ fi
 # this follows the jQuery release technique. derived from: https://stackoverflow.com/a/50974956
 # this is temporary until we migrate to the MuseumofModernArt GH org, and can make use of npm registries
 
+# GH releases
 git checkout master
 git checkout --detach
 yarn build
@@ -18,3 +19,10 @@ git commit -m $1
 git tag v$1
 git push --tags
 git checkout master
+
+# NPM releases
+yarn version --no-git-tag-version --no-commit-hooks --new-version $1
+git add package.json
+git commit -m $1
+git push
+npm publish
