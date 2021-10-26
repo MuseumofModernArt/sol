cedcc647ebfe8f0605f302d1c14f7526247eb03a
Eric Li <eric_li@moma.org>
2021-10-26 17:10:56 -0400

    Nav based anchor offset (#78)
    * add atom for anchor offset
    
    * better nav height calc

---
+++ src/js/nav/nav.js
@@ -0,0 +1,33 @@
+const defaultQuerySelector = 'nav';
+
+export default class Nav {
+  constructor(el) {
+    if (el) {
+      this.navEl = el;
+    } else {
+      this.navEl = document.querySelector(defaultQuerySelector);
+    }
+    
+    this.setCurrentHeightCSS();
+    
+    this.navEl.addEventListener('transitionend', () => {
+      this.setCurrentHeightCSS();
+    });
+
+    window.addEventListener('resize', () => {
+      this.setCurrentHeightCSS();
+    });
+  }
+
+  getCurrentHeight() {
+    if (this.navEl) {
+      return this.navEl.getBoundingClientRect().height + this.navEl.getBoundingClientRect().top;
+    } 
+    return 0;
+  }
+
+  setCurrentHeightCSS() {
+    const height = this.getCurrentHeight();
+    document.documentElement.style.setProperty('--nav-height', `${height}px`);
+  }
+}
